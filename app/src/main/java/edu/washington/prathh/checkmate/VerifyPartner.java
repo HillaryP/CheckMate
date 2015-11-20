c07b3d045881129764e2dc8d490b52d63d58db58
Lily Dwyer <lmdwyer@uw.edu>
2015-11-20 05:01:50 -0800

    small set-up for verify pages

---
+++ app/src/main/java/edu/washington/prathh/checkmate/VerifyPartner.java
@@ -0,0 +1,24 @@
+package edu.washington.prathh.checkmate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+/**
+ * Created by lilymdwyer on 11/20/15.
+ */
+public class VerifyPartner extends Activity{
+
+    public VerifyPartner() {
+        // Required empty public constructor
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_partner);
+    }
+}
