c333189c7e7aaa65758be5e1364f100034bcde0d
Lily Dwyer <lmdwyer@uw.edu>
2015-11-19 19:19:34 -0800

    added tabs to action bar

---
+++ app/src/main/java/edu/washington/prathh/checkmate/TabFragment1.java
@@ -0,0 +1,23 @@
+package edu.washington.prathh.checkmate;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.support.v4.app.Fragment;
+import android.view.ViewGroup;
+
+public class TabFragment1 extends Fragment {
+
+    public TabFragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
+    }
+
+}
