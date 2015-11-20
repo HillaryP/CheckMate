0426fff73c1698fe05e4f2c2b97537f80d7a8a9a
Oscar Wong <wong.s.oscar@gmail.com>
2015-11-20 00:29:07 -0800

    adding app bar to login

--- app/src/main/java/edu/washington/prathh/checkmate/user_flows/Login.java
+++ app/src/main/java/edu/washington/prathh/checkmate/user_flows/Login.java
@@ -5,6 +5,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -34,6 +35,8 @@ public class Login extends ActionBarActivity {
                 login();
             }
         });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void login() {
